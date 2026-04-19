37675077c124acba12c444c0dab7b4d94618a437
eduardolottermann6-tech <eduardolottermann6>
2026-04-19 15:22:44 -0300

    Minha princesa

--- JS/Java.js
+++ JS/Java.js
@@ -1,43 +1,43 @@
-const imagem = ["assets/Foto1.jpeg", "assets/Foto2.jpeg", "assets/Foto3.jpeg", "assets/Foto4.jpeg", "assets/Foto5.jpeg", "assets/Foto6.jpeg", "assets/Foto7.jpeg", "assets/Foto8.jpeg", "assets/Foto9.jpeg", "assets/Foto10.jpeg"];
+const imagens = ["../assets/Foto1.jpeg", "../assets/Foto2.jpeg", "../assets/Foto3.jpeg", "../assets/Foto4.jpeg", "../assets/Foto5.jpeg", "../assets/Foto6.jpeg", "../assets/Foto7.jpeg", "../assets/Foto8.jpeg", "../assets/Foto9.jpeg", "../assets/Foto10.jpeg"];
 let index = 0;
 
 const slide = document.getElementById("slide");
 
-//SLIDESHOW
+// SLIDESHOW
 setInterval(() => {
-    index = (index + 1) % imagem.length;
-    slide.src =  imagem[index];
+  index = (index + 1) % imagens.length;
+  slide.src = imagens[index];
 }, 2500);
 
-//CONTAGEM
+// CONTAGEM
 let tempo = 10;
 const tempoElemento = document.getElementById("tempo");
 const pedido = document.getElementById("pedido");
 const contador = document.getElementById("contador");
 
 const contagem = setInterval(() => {
-    tempo --;
-    tempoElemento.innerText = tempo;
+  tempo--;
+  tempoElemento.innerText = tempo;
 
-    if (tempo <= 0) {
-        clearInterval(contagem);
-        contador.style.display = "none";
-        pedido.classList.remove("hidden");
-    }
+  if (tempo <= 0) {
+    clearInterval(contagem);
+    contador.style.display = "none";
+    pedido.classList.remove("hidden");
+  }
 }, 1000);
 
-//BOTÕES 
-const btnSim = docuement.getElementById("sim");
+// BOTÕES
+const btnSim = document.getElementById("sim");
 const btnNao = document.getElementById("nao");
 const resposta = document.getElementById("resposta");
 
-btnSim.adEventListener("click", () => { 
-    resposta.innerText = "Eu SABIAAA !!! 💖💍";
+btnSim.addEventListener("click", () => {
+  resposta.innerText = "EU SABIA!!! 💖💍";
 });
 
-//botão Não foge
-btnNao.addEventListener("mouseover", () => { 
-    const x = Math.random() * 150 - 75;
-    const y = Math.random() * 150 - 75;
-    btnNao.style.transform = `translate(${x}px, ${y}px)`;
+// botão NÃO foge
+btnNao.addEventListener("mouseover", () => {
+  const x = Math.random() * 150 - 75;
+  const y = Math.random() * 150 - 75;
+  btnNao.style.transform = `translate(${x}px, ${y}px)`;
 });
